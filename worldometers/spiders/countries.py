b537984765d8d2ef885d719c121b57ba5181a5e6
izharabbasi <izharabbasi103@gmail.com>
2020-07-20 16:01:41 +0500

    first commit

---
+++ worldometers/spiders/countries.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class CountriesSpider(scrapy.Spider):
+    name = 'countries'
+    allowed_domains = ['www.worldometers.info']
+    start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
+
+    def parse(self, response):
+        title = response.xpath("//div[@class='col-md-12']/h1/text()").get()
+        countries = response.xpath("//td/a")
+        for country in countries:
+            name = country.xpath(".//text()").get()
+            link = country.xpath(".//@href").get()
+
+            yield {
+                'title': title,
+                'country_name' : name,
+                'country_link': link
+            }
+
+        
